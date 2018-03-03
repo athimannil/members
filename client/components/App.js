92b1194ea225ba5c9b1eaa73e4f2000ee69d5017
Muhammed Athimannil <hello@athimannil.com>
2018-03-03 13:51:22 +0100

    add bootstrap for sass

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +12,19 @@ class App extends React.Component {
     return (
       <main className="main">
         <h1>Hello mate !</h1>
+        <div>
+          <button type="button" className="btn btn-primary">Primary</button>
+          <button
+  type="button"
+  className="btn btn-secondary">Secondary</button>
+          <button type="button" className="btn btn-success">Success</button>
+          <button type="button" className="btn btn-danger">Danger</button>
+          <button type="button" className="btn btn-warning">Warning</button>
+          <button type="button" className="btn btn-info">Info</button>
+          <button type="button" className="btn btn-light">Light</button>
+          <button type="button" className="btn btn-dark">Dark</button>
+          <button type="button" className="btn btn-link">Link</button>
+        </div>
       </main>
     );
   }
