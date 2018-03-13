c088939649d162673301a0f35dd2b2f01328abf6
Muhammed Athimannil <hello@athimannil.com>
2018-03-13 02:18:59 +0100

    appy store

--- client/index.js
+++ client/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './components/App';
 
 import './scss/App.scss';
 
+const store = configureStore();
+
 render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('app'));
